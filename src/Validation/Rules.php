d8e1aa8804e1a2d82ff3d52542fbdadf1df5d011
Otta Levente <leventeotta@gmail.com>
2023-05-03 18:27:33 +0200

    Change validation rules
    No more common funtions

--- src/Validation/Rules.php
+++ src/Validation/Rules.php
@@ -18,20 +18,50 @@ trait Rules
     protected function ruleCheckIn(): array
     {
         return [
+            'guests' => ['required', 'array', 'min:1'],
+            'guests.*.gender' => ['required', new Enum(Gender::class)],
+            'guests.*.guestNumber' => ['required'],
+            'guests.*.touristTaxStatus' => ['required', new Enum(TouristTax::class)],
+            'guests.*.yearOfBirth' => ['required', 'integer', 'min:1900'],
+            'guests.*.residenceCountryCode' => ['required', 'string', 'size:2'],
+            'guests.*.residencePostCode' => ['required', 'string'],
+            'guests.*.nationalityCountryCode' => ['required', 'string', 'size:2'],
+
+            'occupiedResidentialUnit' => ['required', 'array'],
+            'occupiedResidentialUnit.type' => ['required', Rule::enum(ResidentialUnitType::class)],
+            'occupiedResidentialUnit.building' => ['required'],
+            'occupiedResidentialUnit.number' => ['required'],
+            'occupiedResidentialUnit.singleBedCount' => ['required', 'min:0'],
+            'occupiedResidentialUnit.doubleBedCount' => ['required', 'min:0'],
+            'occupiedResidentialUnit.trundleBedCount' => ['required', 'min:0'],
+
             'occurredAt' => ['required', 'date_format:Y-m-d H:i:s'],
             'reservationNumber' => ['required'],
-            ...$this->subRuleGuests(),
-            ...$this->subRuleResidentialUnits('occupiedResidentialUnit'),
         ];
     }
 
     protected function ruleCheckOut(): array
     {
         return [
+            'abandonedResidentialUnit' => ['required', 'array'],
+            'abandonedResidentialUnit.type' => ['required', Rule::enum(ResidentialUnitType::class)],
+            'abandonedResidentialUnit.building' => ['required'],
+            'abandonedResidentialUnit.number' => ['required'],
+            'abandonedResidentialUnit.singleBedCount' => ['required', 'min:0'],
+            'abandonedResidentialUnit.doubleBedCount' => ['required', 'min:0'],
+            'abandonedResidentialUnit.trundleBedCount' => ['required', 'min:0'],
+
+            'guests' => ['required', 'array', 'min:1'],
+            'guests.*.gender' => ['required', new Enum(Gender::class)],
+            'guests.*.guestNumber' => ['required'],
+            'guests.*.touristTaxStatus' => ['required', new Enum(TouristTax::class)],
+            'guests.*.yearOfBirth' => ['required', 'integer', 'min:1900'],
+            'guests.*.residenceCountryCode' => ['required', 'string', 'size:2'],
+            'guests.*.residencePostCode' => ['required', 'string'],
+            'guests.*.nationalityCountryCode' => ['required', 'string', 'size:2'],
+
             'occurredAt' => ['required', 'date_format:Y-m-d H:i:s'],
             'reservationNumber' => ['required'],
-            ...$this->subRuleGuests(),
-            ...$this->subRuleResidentialUnits('abandonedResidentialUnit'),
         ];
     }
 
@@ -141,36 +171,33 @@ trait Rules
     {
         return [
             'arrival' => ['required', 'date_format:Y-m-d'],
-            'departure' => ['required', 'date_format:Y-m-d'],
-            'cancelled' => ['required', 'boolean'],
-            'guestCount' => ['required', 'integer', 'min:1'],
-            'reservationNumber' => ['required'],
-            'marketSegment' => ['required', new Enum(MarketSegment::class)],
-            'reservedAt' => ['required', 'date_format:Y-m-d H:i:s'],
-            'occurredAt' => ['required', 'date_format:Y-m-d H:i:s'],
-            'salesChannel' => ['required', new Enum(SalesChannel::class)],
-            'grossAmount' => ['required', 'numeric', 'min:0'],
-            'nationality' => ['required', 'string', 'size:2'],
             'bookedResidentialUnits' => ['required', 'array', 'min:1'],
             'bookedResidentialUnits.*.type' => ['required', new Enum(ResidentialUnitType::class)],
             'bookedResidentialUnits.*.capacity' => ['required', 'integer', 'min:1'],
+            'cancelled' => ['required', 'boolean'],
+            'departure' => ['required', 'date_format:Y-m-d'],
+            'grossAmount' => ['required', 'numeric', 'min:0'],
+            'guestCount' => ['required', 'integer', 'min:1'],
+            'marketSegment' => ['required', new Enum(MarketSegment::class)],
+            'nationality' => ['required', 'string', 'size:2'],
+            'occurredAt' => ['required', 'date_format:Y-m-d H:i:s'],
+            'reservationNumber' => ['required'],
+            'reservedAt' => ['required', 'date_format:Y-m-d H:i:s'],
+            'salesChannel' => ['required', new Enum(SalesChannel::class)],
         ];
     }
 
     protected function ruleRoomChange(): array
     {
         return [
-            'occurredAt' => ['required', 'date_format:Y-m-d H:i:s'],
-            'reservationNumber' => ['required'],
-            ...$this->subRuleGuests(),
-            ...$this->subRuleResidentialUnits('occupiedResidentialUnit'),
-            ...$this->subRuleResidentialUnits('abandonedResidentialUnit'),
-        ];
-    }
+            'abandonedResidentialUnit' => ['required', 'array'],
+            'abandonedResidentialUnit.type' => ['required', Rule::enum(ResidentialUnitType::class)],
+            'abandonedResidentialUnit.building' => ['required'],
+            'abandonedResidentialUnit.number' => ['required'],
+            'abandonedResidentialUnit.singleBedCount' => ['required', 'min:0'],
+            'abandonedResidentialUnit.doubleBedCount' => ['required', 'min:0'],
+            'abandonedResidentialUnit.trundleBedCount' => ['required', 'min:0'],
 
-    private function subRuleGuests(): array
-    {
-        return [
             'guests' => ['required', 'array', 'min:1'],
             'guests.*.gender' => ['required', new Enum(Gender::class)],
             'guests.*.guestNumber' => ['required'],
@@ -179,19 +206,17 @@ trait Rules
             'guests.*.residenceCountryCode' => ['required', 'string', 'size:2'],
             'guests.*.residencePostCode' => ['required', 'string'],
             'guests.*.nationalityCountryCode' => ['required', 'string', 'size:2'],
-        ];
-    }
 
-    private function subRuleResidentialUnits(string $key): array
-    {
-        return [
-            $key => ['required', 'array'],
-            "$key.type" => ['required', new Enum(ResidentialUnitType::class)],
-            "$key.building" => ['required'],
-            "$key.number" => ['required'],
-            "$key.singleBedCount" => ['required', 'min:0'],
-            "$key.doubleBedCount" => ['required', 'min:0'],
-            "$key.trundleBedCount" => ['required', 'min:0'],
+            'occupiedResidentialUnit' => ['required', 'array'],
+            'occupiedResidentialUnit.type' => ['required', Rule::enum(ResidentialUnitType::class)],
+            'occupiedResidentialUnit.building' => ['required'],
+            'occupiedResidentialUnit.number' => ['required'],
+            'occupiedResidentialUnit.singleBedCount' => ['required', 'min:0'],
+            'occupiedResidentialUnit.doubleBedCount' => ['required', 'min:0'],
+            'occupiedResidentialUnit.trundleBedCount' => ['required', 'min:0'],
+
+            'occurredAt' => ['required', 'date_format:Y-m-d H:i:s'],
+            'reservationNumber' => ['required'],
         ];
     }
 }
